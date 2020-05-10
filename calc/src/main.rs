4384dc0b68f6f2b0ac15dcb491f31b669e9a6d13
Hikotaro Abe <netseed@seedcreate.onmicrosoft.com>
2020-05-10 09:15:19 +0900

    イテレータでいろいろ試した(Main.rs参照)

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -1,13 +1,28 @@
-use calc_core::splitter::split;
+
+fn add_dyn<'a>(scr:&'a mut dyn Iterator<Item=&i32>) -> Box<dyn Iterator<Item=i32>+'a> {
+
+    Box::new(scr.map(|x|x+1))
+}
+
+fn add_impl<'a>(scr:impl Iterator<Item=&'a i32>+'a)->impl Iterator<Item=i32>+'a{
+    scr.map(|x|x+1)
+}
 
 fn main() {
 
-    let out=split("   ");
+    let ite;
 
-    println!("{}",out.len());
 
-    for elem in out.iter().enumerate() {
-        println!("{}:{}",elem.0,elem.1);
+        let v=vec![1,2,3];
+        ite=v.iter();
+
+
+    let ret=add_impl(ite);
+
+    for i in ret {
+        println!("{}",i);
     }
 
+
 }
+
--- calc_core/src/tokenizer.rs
+++ calc_core/src/tokenizer.rs
@@ -1,8 +1,8 @@
 /*
+
 use super::token::Token;
 use super::token::OperatorTypes;
 use std::collections::VecDeque;
+
 */
-
-
 
